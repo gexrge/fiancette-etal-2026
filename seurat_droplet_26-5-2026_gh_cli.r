0d34578a5c0424a0c7718551108db110677005b8
George Hewitt <115406790+gexrge@users.noreply.github.com>
2026-06-05 12:14:56 +0100

    Streamline QC steps for publication

--- seurat_droplet_26-5-2026_gh_cli.r
+++ seurat_droplet_26-5-2026_gh_cli.r
@@ -12,7 +12,7 @@ suppressPackageStartupMessages({
     "patchwork",
     "dplyr",
     "tidyr",
-    "ggplot2", # version 3.5.1
+    "ggplot2", 
     "DoubletFinder",
     "SoupX"
   )
@@ -43,7 +43,6 @@ percent.mt.max <- 20
 
 # ---- Specify paths ----
 path <- here::here()
-projdir <- normalizePath(file.path(path, "../.."))
 indir <- args[2]
 outdir <- args[3]
 if (!dir.exists(indir)) {stop("ERROR: Provided indir does not exist!\n")}
@@ -55,8 +54,6 @@ metadir <- file.path(dirname(indir), "SraRunTable_used.csv")
 source(file.path(path,"utils/seurat_pipe_20-2-2026_gh.r"))
 source(file.path(path,"utils/seurat_doubletfinder_20-2-2026_gh.r"))
 source(file.path(path,"utils/seurat_soupx_22-2-2026_gh.r"))
-source(file.path(path,"utils/seurat_integrate_24-2-2026_gh.r"))
-source(file.path(path,"utils/malat1_function.R"))
 
 # ---- Read in data (organise by experiment) ----
 metadata <- fread(metadir, select = c("Run", "Experiment"))
@@ -97,7 +94,7 @@ crdir_filt <- lapply(unique(metadata$Experiment), function(exp) {
   
   crdir_filt <- crdir_filt[exists]
   
-  # If all directories missing → drop experiment
+  # If all directories missing -> drop experiment
   if (length(crdir_filt) == 0) {
     cat("   WARNING: all directories missing for experiment", exp, "- removing\n")
     return(NULL)
@@ -139,43 +136,23 @@ nobj <- basename(outdir)
 # Initialize list to store Seurat objects
 sobj_list <- list()
 
-# ---- MALAT1, SoupX, MAD-QC, DoubletFinder ----
+# ---- SoupX, QC, DoubletFinder ----
 for (exp in names(crdir_filt)) {
   
   # Create Seurat object name
   cat(">>> Processing", exp, "\n")
   
-  qcdir <- file.path(outdir, paste0(exp, "_QC_out"))
-  dir.create(qcdir, showWarnings = FALSE, recursive = TRUE)
-  qcdir <- normalizePath(qcdir)
+  expdir <- file.path(outdir, paste0(exp, "_QC_out"))
+  dir.create(expdir, showWarnings = FALSE, recursive = TRUE)
+  expdir <- normalizePath(expdir)
   
   # Create pdf for plots (add time to prevent rewrite crash)
-  pdf_path <- file.path(qcdir, paste(nobj, exp, format(Sys.Date(), "%H-%M-%S"), "plots.pdf", sep = "_"))
+  pdf_path <- file.path(expdir, paste(nobj, exp, format(Sys.Date(), "%H-%M-%S"), "plots.pdf", sep = "_"))
   pdf(pdf_path, width = 10, height = 6)
   
   cat("  - Reading in data\n")
   sobj.filt <- Read10X(data.dir = crdir_filt[[exp]])
   
-  # ---- MALAT1 filtering ----
-  # apply malat1 thresholding per experiment: https://github.com/BaderLab/MALAT1_threshold
-  sobj.filt <- CreateSeuratObject(counts = sobj.filt, project = nobj)
-  
-  sobj.filt <- seurat_pipe_20.2.2026_gh(
-    sobj = sobj.filt, 
-    FindNeighbors.dims = FindNeighbors.dims,
-    FindClusters.res = FindClusters.res
-  )
-  
-  norm_counts <- GetAssayData(sobj.filt, assay = "SCT", layer = "data")["MALAT1",]
-  threshold <- define_malat1_threshold_ggplot2(norm_counts)
-  malat1_threshold <- norm_counts > threshold
-  sobj.filt$malat1_threshold <- malat1_threshold
-  sobj.filt$malat1_threshold <- factor(sobj.filt$malat1_threshold, levels = c(TRUE, FALSE))
-  print(DimPlot(sobj.filt, reduction = "umap", group.by = "malat1_threshold"))
-  good_cells <- colnames(sobj.filt)[malat1_threshold]
-  sobj.filt <- subset(sobj.filt, cells = good_cells)
-  
-  # check if raw exists, if so -> SoupX
   if (length(crdir_raw[[exp]]) > 0) {
     
     sobj.raw <- Read10X(data.dir = crdir_raw[[exp]])
@@ -204,7 +181,7 @@ for (exp in names(crdir_filt)) {
       FindClusters.res = FindClusters.res,
       sobj.raw = sobj.raw, 
       sobj.filt = sobj.filt,
-      outdir = qcdir
+      outdir = expdir
     )
     
   } else {
@@ -218,6 +195,7 @@ for (exp in names(crdir_filt)) {
       min.features = 200,
       project = nobj
     )
+    
   }
   
   # ---- Raw QC ----
@@ -226,33 +204,29 @@ for (exp in names(crdir_filt)) {
   # Refilter and add mito to new sobj
   sobj[["percent.mt"]] <- PercentageFeatureSet(sobj, pattern = "^MT-")
   
-  # Visualize QC metrics with violins and scatters
-  print(VlnPlot(sobj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, layer = "counts"))
-  print(FeatureScatter(sobj, feature1 = "nCount_RNA", feature2 = "percent.mt"))
-  print(FeatureScatter(sobj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA"))
-  
-  # Calculate thresholds using logs and Mean Absolute Deviations (MADs)
+  # Calculate thresholds using Mean Absolute Deviations (MADs)
   # https://bioconductor.org/books/3.15/OSCA.basic/quality-control.html#quality-control-outlier
-  sobj$logFeature_RNA <- log1p(sobj$nFeature_RNA)
-  MAD_feats.min <- median(sobj$logFeature_RNA) - MAD_devs * mad(sobj$logFeature_RNA)
-  logFeature_RNA.min <- max(200, MAD_feats.min) # clamp lower threshold (match to seurat object creation)
-  logFeature_RNA.max <- median(sobj$logFeature_RNA) + MAD_devs * mad(sobj$logFeature_RNA)
+  MAD_feats.min <- median(sobj$nFeature_RNA) - MAD_devs * mad(sobj$nFeature_RNA)
+  nFeature_RNA.min <- max(200, MAD_feats.min) # clamp lower threshold (match to seurat object creation)
+  nFeature_RNA.max <- median(sobj$nFeature_RNA) + MAD_devs * mad(sobj$nFeature_RNA)
   
-  sobj$logCount_RNA <- log1p(sobj$nCount_RNA)
-  MAD_count.min <- median(sobj$logCount_RNA) - MAD_devs * mad(sobj$logCount_RNA)
-  logCount_RNA.min <- max(0, MAD_count.min) # clamp lower threshold (feature filtering should capture cells with really low counts)
-  logCount_RNA.max <- median(sobj$logCount_RNA) + MAD_devs * mad(sobj$logCount_RNA)
+  MAD_count.min <- median(sobj$nCount_RNA) - MAD_devs * mad(sobj$nCount_RNA)
+  nCount_RNA.min <- max(0, MAD_count.min) # clamp lower threshold (feature filtering should capture cells with really low counts)
+  nCount_RNA.max <- median(sobj$nCount_RNA) + MAD_devs * mad(sobj$nCount_RNA)
   
-  p.feat <- VlnPlot(sobj, features = "logFeature_RNA", layer = "counts") +
-    geom_hline(yintercept = logFeature_RNA.min, linetype = "dashed", color = "darkblue") +
-    geom_hline(yintercept = logFeature_RNA.max, linetype = "dashed", color = "tomato") +
-    ggtitle("logFeature_RNA") +
+  #Visualize QC metrics with violins and scatters
+  print(VlnPlot(sobj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, layer = "counts"))
+  
+  p.feat <- VlnPlot(sobj, features = "nFeature_RNA", layer = "counts") +
+    geom_hline(yintercept = nFeature_RNA.min, linetype = "dashed", color = "darkblue") +
+    geom_hline(yintercept = nFeature_RNA.max, linetype = "dashed", color = "tomato") +
+    ggtitle("nFeature_RNA") +
     NoLegend()
   
-  p.count <- VlnPlot(sobj, features = "logCount_RNA", layer = "counts") +
-    geom_hline(yintercept = logCount_RNA.min, linetype = "dashed", color = "darkblue") +
-    geom_hline(yintercept = logCount_RNA.max, linetype = "dashed", color = "tomato") +
-    ggtitle("logCount_RNA") +
+  p.count <- VlnPlot(sobj, features = "nCount_RNA", layer = "counts") +
+    geom_hline(yintercept = nCount_RNA.min, linetype = "dashed", color = "darkblue") +
+    geom_hline(yintercept = nCount_RNA.max, linetype = "dashed", color = "tomato") +
+    ggtitle("nCount_RNA") +
     NoLegend()
   
   p.mt <- VlnPlot(sobj, features = "percent.mt", layer = "counts") +
@@ -262,13 +236,18 @@ for (exp in names(crdir_filt)) {
   
   print(p.feat | p.count | p.mt)
   
+  print(FeatureScatter(sobj, feature1 = "nCount_RNA", feature2 = "percent.mt"))
+  print(FeatureScatter(sobj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA"))
+  
   # Remove cells that fail QC
   num_cells_preQC <- ncol(sobj)
-  sobj <- subset(sobj, subset = 
-      logFeature_RNA > logFeature_RNA.min & 
-      logFeature_RNA < logFeature_RNA.max &
-      logCount_RNA > logCount_RNA.min &
-      logCount_RNA < logCount_RNA.max &
+  sobj <- subset(
+    sobj, 
+    subset = 
+      nFeature_RNA > nFeature_RNA.min & 
+      nFeature_RNA < nFeature_RNA.max &
+      nCount_RNA > nCount_RNA.min &
+      nCount_RNA < nCount_RNA.max &
       percent.mt < percent.mt.max
   )
   num_cells_postQC <- ncol(sobj)
@@ -277,17 +256,17 @@ for (exp in names(crdir_filt)) {
   
   # save qc stats per experiment
   params <- list(
-    logFeature_RNA.min = logFeature_RNA.min,
-    logFeature_RNA.max = logFeature_RNA.max,
-    logCount_RNA.min = logCount_RNA.min,
-    logCount_RNA.max = logCount_RNA.max,
+    nFeature_RNA.min = nFeature_RNA.min,
+    nFeature_RNA.max = nFeature_RNA.max,
+    nCount_RNA.min = nCount_RNA.min,
+    nCount_RNA.max = nCount_RNA.max,
     num_cells_preQC = num_cells_preQC,
     num_cells_postQC = num_cells_postQC,
     percent_cells_kept = percent_cells_kept
   )
   
   df <- data.frame(name = names(params), value = unlist(params), row.names = NULL)
-  write.table(df, file = file.path(qcdir, paste(nobj,exp,"QC_stats.txt", sep="_")), quote = FALSE, sep = "\t", row.names = FALSE)
+  write.table(df, file = file.path(expdir, paste(nobj,exp,"QC_stats.txt", sep="_")), quote = FALSE, sep = "\t", row.names = FALSE)
   
   # runs SCT, PCA, neighbours, clusters, UMAP
   sobj <- seurat_pipe_20.2.2026_gh(
@@ -296,17 +275,11 @@ for (exp in names(crdir_filt)) {
     FindClusters.res = FindClusters.res
   )
   
-  # Plot highly variable features per experiment
-  # (avoids multi model clashes after integration)
-  top10 <- head(VariableFeatures(sobj), 10)
-  VFplot <- VariableFeaturePlot(sobj)
-  print(LabelPoints(plot = VFplot, points = top10, repel = TRUE))
-  
   # ---- Run doubletfinder helper function ----
   sobj <- seurat_doubletfinder_20.2.2026_gh(
     sobj = sobj, 
     nobj = nobj,
-    outdir = qcdir,
+    outdir = expdir,
     FindNeighbors.dims = FindNeighbors.dims,
     seq_method = "droplet"
   )
@@ -324,137 +297,30 @@ for (exp in names(crdir_filt)) {
 pdf_path <- file.path(outdir, paste(nobj, format(Sys.Date(), "%H-%M-%S"), "plots.pdf", sep = "_"))
 pdf(pdf_path, width = 10, height = 6)
 
-# ---- Integrate data ----
+# ---- merge data ----
 if (length(sobj_list) > 1) {
-  # runs merging, seurat pipeline, integration, and pipeline again
-  merged <- seurat_integrate_24.2.2026_gh(
-    sobj_list = sobj_list,
-    nobj = nobj,
-    FindClusters.res = FindClusters.res,
-    FindNeighbors.dims = FindNeighbors.dims
+  
+  # runs merging
+  merged <- merge(
+    x = sobj_list[[1]],
+    y = sobj_list[-1],
+    add.cell.ids = names(sobj_list),
+    project = nobj
   )
+
+  # re-join individually QCed sequencing libraries
+  merged[["RNA"]] <- JoinLayers(merged[["RNA"]])
+  
 } else if (length(sobj_list) == 1) {
-  # runs sctransform, pca, neighbours, clusters, umap
-  merged <- seurat_pipe_20.2.2026_gh(
-    sobj = sobj_list[[1]],
-    FindNeighbors.dims = FindNeighbors.dims,
-    FindClusters.res = FindClusters.res
-  )
+  
+  # take first sobj, no individual counts to join
+  merged <- sobj_list[[1]]
+  
 } else {
   stop("ERROR: no Seurat objects found")
 }
 
-# Report variable features and plot dimensionality reduction plots
-print(VizDimLoadings(merged, dims = 1:2, reduction = "pca"))
-print(DimPlot(merged, reduction = "pca", raster = FALSE) + NoLegend())
-print(DimHeatmap(merged, dims = 1, cells = 500, balanced = TRUE))
-print(DimPlot(merged, reduction = "umap", raster = FALSE))
-print(DimPlot(merged, reduction = "umap", raster = FALSE, label = TRUE)+ NoLegend())
-
-# ---- Plotting markers ----
-# Highlight genes of interest/markers
-all_markers_list  <- list(
-  duct =  c("SOX9", "KRT19", "CFTR", "MUC1"), 
-  acin =  c("CPA1", "PRSS1", "CTRC", "AMY2B"),
-  vasc =  c("PDGFRB", "PECAM1", "VWF", "RGS5"), # endothelial, pericytes
-  imm =   c("MS4A1", "LYZ", "PTPRC", "CD68"), # macrophages, T/B cells
-  strom = c("COL1A1", "PDGFRA", "DCN", "COL3A1"), # fibroblasts, stellate
-  endoc =  c("ISL1", "NEUROD1", "PDX1", "CHGA"),
-  horm = c("INS", "GCG", "SST", "PPY", "GHRL"),
-  empd =  c("MALAT1", "nFeature_RNA", "nCount_RNA", "percent.mt")
-)
-
-# filter all markers to remove warning messages
-allowed <- union(rownames(merged), colnames(merged@meta.data))
-all_markers_filt <- lapply(
-  all_markers_list, 
-  function(g) intersect(g, allowed)
-)
-all_markers_filt <- Filter(length, all_markers_filt)
-
-# show marker expression per cluster
-for (markers in all_markers_filt) {
-  print(FeaturePlot(merged, features = markers, reduction = "umap", raster = FALSE))
-  print(VlnPlot(merged, features = markers))
-}
-
-# dotplot, fix gene names
-print(
-  DotPlot(merged, features = all_markers_filt) + 
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-)
-
-# ---- Calculate cluster stats ----
-# Number and percentage of cells per cluster
-per_cluster_stats <- 
-  table(Idents(merged)) %>% 
-  as.data.frame() %>% 
-  rename(cluster = Var1, cell_count = Freq) %>% 
-  mutate(percentage = round((cell_count/ncol(merged))*100, 2))
-write.table(
-  per_cluster_stats,
-  file = file.path(outdir, paste0(nobj, "_per_cluster_stats.txt")),
-  quote = FALSE,
-  row.names = FALSE,
-  sep = "\t"
-)
-
-# Finding differentially expressed features
-if (length(merged[["SCT"]]@SCTModel.list) > 1) {
-  cat("  - Detected multi-model SCT object, prepping before FindAllMarkers()\n")
-  merged <- PrepSCTFindMarkers(merged, verbose = FALSE)
-}
-
-merged.markers <- FindAllMarkers(merged, only.pos = TRUE, verbose = FALSE)
-write.table(
-  merged.markers, 
-  file = file.path(outdir, paste0(nobj,"_allmarkers.txt")),
-  quote = FALSE,
-  row.names = FALSE,
-  sep = "\t"
-)
-
-# heatmap top 5 DEGs per cluster 
-top5 <- 
-  merged.markers %>%
-  group_by(cluster) %>%
-  dplyr::filter(avg_log2FC > 1) %>%
-  slice_head(n = 5) %>%
-  ungroup()
-print(DoHeatmap(merged, features = top5$gene) + NoLegend())
-
-# Save significantly expressed genes
-signif_markers <- 
-  merged.markers %>% 
-  group_by(cluster) %>% 
-  dplyr::filter(avg_log2FC > 1, p_val_adj < 0.05)
-write.table(
-  signif_markers,
-  file = file.path(outdir, paste0(nobj,"_adjpval0.05_logfc1_markers.txt")),
-  quote = FALSE,
-  row.names = FALSE,
-  sep = "\t"
-)
-
-# Save top 50 markers per cluster for GSEA/GO
-ranked_top50 <- merged.markers %>%
-  filter(p_val_adj < 0.05) %>% 
-  group_by(cluster) %>%
-  arrange(desc(avg_log2FC), .by_group = TRUE) %>% 
-  mutate(rank = row_number()) %>% 
-  slice_head(n = 50) %>%
-  ungroup() %>% 
-  select(cluster, rank, gene) %>%
-  pivot_wider(names_from = cluster, values_from = gene)
-write.table(
-  ranked_top50,
-  file = file.path(outdir, paste0(nobj,"_ranked_top50_markers.txt")),
-  quote = FALSE,
-  row.names = FALSE,
-  sep = "\t"
-)
-
-# ---- Save clustered and annotated sobj ----
+# ---- Save merged sobj ----
 cat(">>> Saving final Seurat object\n")
 dev.off()
 saveRDS(merged, file = file.path(outdir, paste0(nobj,"_merged.rds")))
